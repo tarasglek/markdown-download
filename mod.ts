7fece4082edb64f9c371fc5d9bef2479a4be4170
Taras Glek <taras@glek.net>
2024-03-24 12:11:14 +0200

    misc

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 // Import the default export from the module
-import defaultExcodeporterHandler from "./dist/scrape2md.ts";
+import defaultExcodeporterHandler from "https://esm.town/v/taras/scrape2md?v=67";
 
 // Re-export the imported default export under the name `serve`
 export { defaultExcodeporterHandler as serve };
